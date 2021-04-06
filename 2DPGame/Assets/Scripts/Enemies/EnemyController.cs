f34d57cced5a7dd3d12a28498afc89237ca29b74
H1Karahan <h1-hknkrhn@outlook.com>
2021-04-06 23:44:38 +0300

    Update2

---
+++ 2DPGame/Assets/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private enum State
+    {
+        Walking,
+        Knockback,
+        Dead
+    }
+
+    private State currentState;
+
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                UpdateWalkingState();
+                break;
+            case State.Knockback:
+                UpdateKnockbackState();
+                break;
+            case State.Dead:
+                UpdateDeadState();
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    //Walking State
+
+    private void EnterWalkingState()
+    {
+
+    }
+
+    private void UpdateWalkingState()
+    {
+
+    }
+
+    private void ExitWalkingState()
+    {
+
+    }
+
+    //Knockback State
+
+    private void EnterKnockbackState()
+    {
+
+    }
+
+    private void UpdateKnockbackState()
+    {
+
+    }
+
+    private void ExitKnockbackState()
+    {
+
+    }
+
+    //Dead State
+
+    private void EnterDeadState()
+    {
+
+    }
+
+    private void UpdateDeadState()
+    {
+
+    }
+
+    private void ExitDeadState()
+    {
+
+    }
+
+    //OtherFunc.
+
+    private void SwitchState(State state)
+    {
+        switch (currentState)
+        {
+            case State.Walking:
+                ExitWalkingState();
+                break;
+            case State.Knockback:
+                ExitKnockbackState();
+                break;
+            case State.Dead:
+                ExitDeadState();
+                break;
+            default:
+                break;
+        }
+
+        switch (state)
+        {
+            case State.Walking:
+                EnterWalkingState();
+                break;
+            case State.Knockback:
+                EnterKnockbackState();
+                break;
+            case State.Dead:
+                EnterDeadState();
+                break;
+            default:
+                break;
+        }
+
+        currentState = state;
+    }
+}
