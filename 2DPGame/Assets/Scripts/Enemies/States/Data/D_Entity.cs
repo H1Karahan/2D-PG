b1f64b8b6bf98782aef612fcedf5bb01f950faf1
H1Karahan <h1-hknkrhn@outlook.com>
2021-04-17 16:43:41 +0300

    Update4 Inheritance - 1

---
+++ 2DPGame/Assets/Scripts/Enemies/States/Data/D_Entity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newEntityData", menuName = "Data/Entity Data/Base Data")]
+public class D_Entity : ScriptableObject
+{
+    public float wallCheckDistance = 2f;
+    public float ledgeCheckDistance = 0.4f;
+
+    public float maxAgroDistance = 4;
+    public float minAgroDistance = 3;
+
+    public LayerMask whatIsGround;
+    public LayerMask whatIsPlayer;
+}
---
+++ 2DPGame/Assets/Scripts/Enemies/States/Data/D_PlayerDetected.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newPlayerDetectedStateName", menuName= "Data/State Data/Player Detected Data")]
+public class D_PlayerDetected : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
